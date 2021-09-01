e0bd37a8f62a0426834cfb98f53efee1886156c3
Shariar Raktim <raktim21cse@gmail.com>
2021-09-02 00:36:59 +0600

    eCommerce

--- resources/views/livewire/product-details-component.blade.php
+++ resources/views/livewire/product-details-component.blade.php
@@ -58,7 +58,7 @@
                                 </div>
                             </div>
                             <div class="wrap-butons">
-                                <a href="#" class="btn add-to-cart">Add to Cart</a>
+                                <a href="#" class="btn add-to-cart" wire:click.prevent='store({{$details->id}})'>Add to Cart</a>
                                 <div class="wrap-btn">
                                     <a href="#" class="btn btn-compare">Add Compare</a>
                                     <a href="#" class="btn btn-wishlist">Add Wishlist</a>
@@ -317,6 +317,5 @@
 
         </div>
         <!--end container-->
-
     </main>
 </div>
