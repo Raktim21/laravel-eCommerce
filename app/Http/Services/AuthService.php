37e9fb89f1156c54c2f622d6b6745c901f7d2324
nnawar706 <nnawar706@gmail.com>
2023-08-28 19:09:29 +0600

    send-wishlist

--- app/Http/Services/AuthService.php
+++ app/Http/Services/AuthService.php
@@ -14,7 +14,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
