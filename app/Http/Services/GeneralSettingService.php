9941d11cb10fe78e92bb1868a711e59e258d60b8
nnawar706 <nnawar706@gmail.com>
2023-10-30 13:49:21 +0600

    general-setting-drop-columns

--- app/Http/Services/GeneralSettingService.php
+++ app/Http/Services/GeneralSettingService.php
@@ -35,14 +35,6 @@ class GeneralSettingService
         $this->setting->google = $request->google ?? $this->setting->google;
         $this->setting->pinterest = $request->pinterest ?? $this->setting->pinterest;
         $this->setting->about = $request->about ?? $this->setting->about;
-        $this->setting->theme_color = $request->theme_color ?? $this->setting->theme_color;
-        $this->setting->text_color = $request->text_color ?? $this->setting->text_color;
-        $this->setting->badge_background_color = $request->badge_background_color ?? $this->setting->badge_background_color;
-        $this->setting->badge_text_color = $request->badge_text_color ?? $this->setting->badge_text_color;
-        $this->setting->button_color = $request->button_color ?? $this->setting->button_color;
-        $this->setting->button_text_color = $request->button_text_color ?? $this->setting->button_text_color;
-        $this->setting->price_color = $request->price_color ?? $this->setting->price_color;
-        $this->setting->discount_price_color = $request->discount_price_color ?? $this->setting->discount_price_color;
         $this->setting->save();
 
         if ($request->hasFile('logo'))
